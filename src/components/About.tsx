9685e66dd2b3b9368d9efbb9300b171ef5c525bf
Jocelyn <jlcasto@gmail.com>
2019-09-27 15:08:37 -0400

    updating bio section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,17 +12,16 @@ const About = () => {
           </div>
           <div className='col-lg-6'>
             <p>
-              I am a software engineer with over twelve years of experience. I am passionate about developing intuitive
-              user interfaces that are easy to maintain and update.
+            I am a software engineer with over twelve years of experience. I am passionate about solving human-centric problems.  That includes developing intuitive user interfaces as well as improving development processes, following clean code practices so that others are able to maintain my code, and creating collaborative team cultures where people enjoy coming to work.
             </p>
             <p>
-              I have a breadth of experience developing rapid prototypes, maturing prototypes to production software,
-              improving and maintaining existing production systems. as well as experience leading agile teams under
-              tight deadlines
+            I have a breadth of experience developing rapid prototypes, maturing prototypes to production software, improving and maintaining existing production systems. as well as experience leading agile teams under tight deadlines
             </p>
             <p>
-              I am well versed in Lean product development, agile software development practices, requirements
-              gathering, and generating mockups.
+            I am well versed in Lean product development, agile software development practices, requirements gathering, and generating mockups.  I enjoy working with Typescript and React and am very interested in learning Vue.js.
+            </p>
+            <p>
+            I am interested in giving technical talks to our local community.  Please reach out with a when, where, and general topic if you are interested in inviting me to your event. 
             </p>
           </div>
           <div className='col-lg-3'>
