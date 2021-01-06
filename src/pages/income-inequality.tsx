0e8a1dd1fce67ad8e3b962c11d278c2d35b89db5
Jocelyn Irwin <jlcasto@gmail.com>
2021-01-05 20:04:52 -0500

    No blank pages allowed, so added a stub

---
+++ src/pages/income-inequality.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import * as moment from 'moment';
+import { Helmet } from 'react-helmet';
+
+interface IncomeInequalityPageProps {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string;
+        description: string;
+        canonical: string;
+      };
+    };
+  };
+}
+
+export default class IncomeInequalityPage extends React.Component<IncomeInequalityPageProps, {}> {
+  public render() {
+    return (
+      <div className={'container-fluid'}>
+        <Helmet htmlAttributes={{ lang: 'en' }}>
+          <meta charSet='utf-8' />
+          <title>{this.props.data.site.siteMetadata.title}</title>
+          <link rel='canonical' href={this.props.data.site.siteMetadata.canonical} />
+          <meta name='description' content={this.props.data.site.siteMetadata.description} />
+        </Helmet>
+        <div className={`row justify-content-center`}>
+          <h1>Income Inequality Infographic</h1>
+          <p>Coming Soon</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export const query = graphql`
+  query {
+    site: site {
+      siteMetadata {
+        title
+        description
+        canonical
+      }
+    }
+  }
+`;
