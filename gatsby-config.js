2b6743120090bccadc27a899ffc9db0d93c9bd35
Jocelyn Irwin <jlcasto@gmail.com>
2021-01-05 20:42:23 -0500

    minor change: updating google id again

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-S5Q1TY8B2P", // Google Analytics ID
+          "257417247", // Google Analytics ID
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
