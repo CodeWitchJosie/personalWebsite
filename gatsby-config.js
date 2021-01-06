0ce747a23f55a9b764d11daa4a18e3996a4c2547
Jocelyn Irwin <jlcasto@gmail.com>
2021-01-05 20:44:10 -0500

    minor change again: publish with both google tracking id's till i figure out google analytics better

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,8 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "257417247", // Google Analytics ID
+          "G-S5Q1TY8B2P", // Google Analytics ID
+          "257417247", 
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
