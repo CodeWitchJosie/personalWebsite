a6828a9033ae8b1392c300e2bd34d40500d2cc5e
Jocelyn Irwin <726273+CodeWitchJosie@users.noreply.github.com>
2023-11-22 13:40:43 -0500

    necessary for gatsby build

--- gatsby-node.mjs
+++ gatsby-node.mjs
@@ -1,7 +1,5 @@
-// const ChildProcess = require('child_process');
-
-import { execSync } from 'child_process';
+// import { execSync } from 'child_process';
 
 export const onPostBuild = () => {
-  execSync("ps aux | grep jest | grep -v grep | awk '{print $2}' | xargs kill");
+  // execSync("ps aux | grep jest | grep -v grep | awk '{print $2}' | xargs kill");
 };
